f4bc89276244561682c1f1f938e49646662d11b4
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-28 16:24:53 +0200

    aggiunto tocco di classe

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const app = new Vue({
     el: "#app",
     data: {
         title: "HelloWorld, I'm the first app in VueJS made by AnTosi!!!",
-        picture: "./img/img.jpg"
+        picture: "./img/img.jpg",
+        userPic: "",
+        
+        
     }
+    
 })
+
+
