c80c1f18804f9c856a15097b17c78eefa3cc4ace
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-28 15:18:48 +0200

    non riesco a caricare l'immagine, non trovo tra la documentazione

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+let image = "./img/img.jpg";
+
+
 const app = new Vue({
     el: "#app",
     data: {
         title: "HelloWorld, I'm the first app in VueJS made by AnTosi!!!",
-        picture: ""
+        picture: image
     }
 })
