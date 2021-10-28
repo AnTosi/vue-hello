cb38a596c26e2c65247468394ada14501432f19e
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-28 16:12:04 +0200

    aggiunto commenti

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+//dopo aver collegato html a cdn creo nuovo vue
+//ci collego un elemento della dom
+//creo dei data per quell'elemento
+//prima inserisco il mio messaggio con i mustacchi per esercizio base
+//per il bonus dopo diversi tentativi sono riuscito a rendere interattivo il tag src(mi incaponivo o a scrivere tutto l'html qua dentro Vue o a inserire il mio data dentro al src ma senza il v-bind non funzionava)
 
 const app = new Vue({
     el: "#app",
